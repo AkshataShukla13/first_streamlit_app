9d00212b0c8826acc11c9c76fb7c316d89f8aea3
AkshataShukla13 <139464084+AkshataShukla13@users.noreply.github.com>
2023-07-14 14:17:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.header('🥣 Breakfast menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale,Spinach and Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled free-range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
